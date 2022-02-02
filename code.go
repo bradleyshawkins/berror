805e0a8ec7b0bda96758544c8bb49dd49cf6d6a4
Brad Hawkins <bradleyshawkins@gmail.com>
2022-02-01 21:31:50 -0700

    Add error type, options and codes

---
+++ code.go
@@ -0,0 +1,29 @@
+package error
+
+import "net/http"
+
+type Code int
+
+const (
+	CodeUnknown Code = iota + 1
+	CodeUnauthenticated
+	CodeInvalidPayload
+	CodeInvalidField
+	CodeInternal
+	CodeDuplicate
+	CodeNotExists
+	CodeRequiredEntityNotExists
+	CodeDisabled
+)
+
+var codeHttpStatusCodeMap = map[Code]int{
+	CodeUnknown:                 http.StatusInternalServerError,
+	CodeUnauthenticated:         http.StatusUnauthorized,
+	CodeInvalidField:            http.StatusBadRequest,
+	CodeInternal:                http.StatusInternalServerError,
+	CodeDuplicate:               http.StatusConflict,
+	CodeInvalidPayload:          http.StatusBadRequest,
+	CodeNotExists:               http.StatusNotFound,
+	CodeRequiredEntityNotExists: http.StatusConflict,
+	CodeDisabled:                http.StatusNotFound,
+}
---
+++ option.go
@@ -0,0 +1,77 @@
+package error
+
+const (
+	UnauthenticatedMsg         = "user is not authenticated"
+	InvalidFieldMsg            = "Invalid field was provided"
+	InternalMsg                = "Unexpected internal error occurred"
+	DuplicateMsg               = "Entity already exists"
+	InvalidPayloadMsg          = "Unable to deserialize payload"
+	NotExistsMsg               = "Entity does not exist"
+	RequiredEntityNotExistsMsg = "A required entity has not been created"
+	DisabledMsg                = "Entity has been disabled"
+)
+
+type Option func(e *Error)
+
+func WithUnauthenticated() Option {
+	return func(e *Error) {
+		e.code = CodeUnauthenticated
+		e.userMessage = UnauthenticatedMsg
+	}
+}
+
+func WithInternal() Option {
+	return func(e *Error) {
+		e.code = CodeInternal
+		e.userMessage = InternalMsg
+	}
+}
+
+func WithInvalidFields(invalidFields ...InvalidField) Option {
+	return func(e *Error) {
+		e.code = CodeInvalidField
+		e.userMessage = InvalidFieldMsg
+		e.fields = invalidFields
+	}
+}
+
+func WithDuplicate() Option {
+	return func(e *Error) {
+		e.code = CodeDuplicate
+		e.userMessage = DuplicateMsg
+	}
+}
+
+func WithNotExists() Option {
+	return func(e *Error) {
+		e.code = CodeNotExists
+		e.userMessage = NotExistsMsg
+	}
+}
+
+func WithInvalidPayload() Option {
+	return func(e *Error) {
+		e.code = CodeInvalidPayload
+		e.userMessage = InvalidPayloadMsg
+	}
+}
+
+func WithRequiredEntityNotExist() Option {
+	return func(e *Error) {
+		e.code = CodeRequiredEntityNotExists
+		e.userMessage = RequiredEntityNotExistsMsg
+	}
+}
+
+func WithMessage(message string) Option {
+	return func(e *Error) {
+		e.message = message
+	}
+}
+
+func WithEntityDisabled() Option {
+	return func(e *Error) {
+		e.code = CodeDisabled
+		e.userMessage = DisabledMsg
+	}
+}
